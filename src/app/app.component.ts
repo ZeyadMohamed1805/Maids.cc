408a1b7cd4f6662a7e057ca6e5a2b9e75b6dfa97
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 02:10:30 +0300

    Included The Layout Components In The Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
-	template: '<router-outlet />',
+	imports: [RouterOutlet, HeaderComponent, FooterComponent],
+	template: `
+		<app-header />
+		<router-outlet />
+		<app-footer />
+	`,
 })
 export class AppComponent {}
