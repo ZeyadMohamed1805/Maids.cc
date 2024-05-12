9ff8c95ed3d66128f13d5314743cf71bc91382de
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 02:04:40 +0300

    Added The Users Page To The Router System

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'users' },
+	{ component: UsersComponent, path: 'users' },
+];
