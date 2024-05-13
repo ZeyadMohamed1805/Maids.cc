b37aca008d598bc70e8531c357c2895a6b63f302
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 18:36:07 +0300

    Completed The Users Service

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,1 +1,7 @@
-export interface IUser {}
+import { TUser } from '../types/user';
+import { Observable } from 'rxjs';
+
+export interface IUserService {
+	getUsersPage(page: number): Observable<TUser[]>;
+	getUserDetails(id: number): Observable<TUser>;
+}
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { IUserService } from '../../interfaces/user';
+import { TUser } from '../../types/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
-export class UserService {
+export class UserService implements IUserService {
+	constructor(private apiService: ApiService) {}
 
-  constructor() { }
+	getUsersPage(page: number): Observable<TUser[]> {
+		return this.apiService.get<TUser[]>(`/users?page=${page}`);
+	}
+
+	getUserDetails(id: number): Observable<TUser> {
+		return this.apiService.get<TUser>(`users/${id}`);
+	}
 }
