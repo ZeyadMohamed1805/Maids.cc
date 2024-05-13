aa293103ede21a2243e0065b2ff15ca7254e2453
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-14 02:12:07 +0300

    Completed The Details Page's Return Component

--- src/app/components/details/return/return.component.ts
+++ src/app/components/details/return/return.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-return',
-  standalone: true,
-  imports: [],
-  templateUrl: './return.component.html',
-  styleUrl: './return.component.scss'
+	selector: 'app-return',
+	standalone: true,
+	imports: [],
+	templateUrl: './return.component.html',
+	styleUrl: './return.component.scss',
 })
 export class ReturnComponent {
+	constructor(private router: Router) {}
 
+	onGoBackClick(): void {
+		this.router.navigateByUrl('users');
+	}
 }
