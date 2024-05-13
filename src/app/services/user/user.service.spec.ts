e385b39244cd49c7e7d0bf15a9bda1f38495c201
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 18:49:12 +0300

    Initially Completed The GetUsersPage Unit Test

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { Observable } from 'rxjs';
+import { TUserResponse } from '../../types/user';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UserService', () => {
-  let service: UserService;
+	let service: UserService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+		service = TestBed.inject(UserService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	describe('getUsersPage', () => {
+		it('should return an observable response type', () => {
+			const expected = new Observable<TUserResponse>();
+			const received = service.getUsersPage(1);
+
+			expect(typeof expected).toEqual(typeof received);
+		});
+	});
 });
