a6fce8d13e2b5c669e480b1300bca559e1b197d6
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-14 02:18:32 +0300

    Added The User Component To The Details Page

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ReturnComponent } from '../../components/details/return/return.component';
+import { MatDivider } from '@angular/material/divider';
+import { UserComponent } from '../../components/details/user/user.component';
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [ReturnComponent],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+	selector: 'app-details',
+	standalone: true,
+	imports: [MatDivider, ReturnComponent, UserComponent],
+	templateUrl: './details.component.html',
+	styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
-
-}
+export class DetailsComponent {}
