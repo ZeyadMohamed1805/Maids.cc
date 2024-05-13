19c9cec1d3383c1fd7b47a3f86886da65c802c0f
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 19:32:57 +0300

    Completed Using The User Service In The Users Page's Main Component

--- src/app/components/users/main/main.component.ts
+++ src/app/components/users/main/main.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { CardComponent } from '../card/card.component';
+import { UserService } from '../../../services/user/user.service';
+import { TUser, TUserResponse } from '../../../types/user';
 
 @Component({
 	selector: 'app-main',
@@ -9,8 +11,23 @@ import { CardComponent } from '../card/card.component';
 	templateUrl: './main.component.html',
 	styleUrl: './main.component.scss',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 	searching: boolean = false;
+	response: TUserResponse | null = null;
 
-	onPageClick(event: any) {}
+	constructor(private userService: UserService) {}
+
+	ngOnInit(): void {
+		this.getUsers(1);
+	}
+
+	onPageClick(event: any): void {
+		this.getUsers(event.pageIndex + 1);
+	}
+
+	getUsers(page: number): void {
+		this.userService.getUsersPage(page).subscribe((response) => {
+			this.response = response;
+		});
+	}
 }
