c0cd7f144fc43946ebe3ebd8d3e75611724a8999
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 03:14:31 +0300

    Completed The Header Layout Component's Template

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { MatInput } from '@angular/material/input';
+import {
+	MatAutocomplete,
+	MatAutocompleteTrigger,
+	MatOption,
+} from '@angular/material/autocomplete';
+import { RouterLink } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+	selector: 'app-header',
+	standalone: true,
+	imports: [
+		RouterLink,
+		TitleCasePipe,
+		MatInput,
+		MatAutocomplete,
+		MatAutocompleteTrigger,
+		MatOption,
+	],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+	searching: boolean = false;
+	navLinks: string[] = ['home', 'users'];
+	options: string[] = ['One', 'Two', 'Three'];
 
+	onSearchSubmit(searchValue: string) {
+		console.log(searchValue);
+	}
+	onSearchChange(searchValue: string) {
+		console.log(searchValue);
+	}
 }
