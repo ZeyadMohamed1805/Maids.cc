5ebd9dabf566b0812be8125c619ddbd8f9452042
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 04:39:33 +0300

    Installed Node Types For Environmental Variable Access

--- src/app/services/api/api.service.spec.ts
+++ src/app/services/api/api.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 describe('ApiService', () => {
-  let service: ApiService;
+	let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(ApiService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	describe('get', () => {
+		it('should return an observable of the generic type inserted', () => {
+			const expected = new Observable<Array<any>>();
+			const data = typeof service.get<Array<any>>('users');
+
+			expect(typeof data).toEqual(typeof expected);
+		});
+	});
 });
