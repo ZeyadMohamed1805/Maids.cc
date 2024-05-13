c492928c0903726f0830561f5bca1e50b8b0fa85
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-13 03:43:14 +0300

    Completed The Footer Layout Component Template

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatDivider } from '@angular/material/divider';
+import {
+	MatAutocomplete,
+	MatAutocompleteTrigger,
+	MatOption,
+} from '@angular/material/autocomplete';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+	selector: 'app-footer',
+	standalone: true,
+	imports: [
+		TitleCasePipe,
+		RouterLink,
+		MatDivider,
+		MatAutocomplete,
+		MatAutocompleteTrigger,
+		MatOption,
+	],
+	templateUrl: './footer.component.html',
+	styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+	searching: boolean = false;
+	footerLinks: string[] = ['home', 'users'];
+	options: string[] = ['One', 'Two', 'Three'];
 
+	onSearchSubmit(searchValue: string) {
+		console.log(searchValue);
+	}
+	onSearchChange(searchValue: string) {
+		console.log(searchValue);
+	}
 }
