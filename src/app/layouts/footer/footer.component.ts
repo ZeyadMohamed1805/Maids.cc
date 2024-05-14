539d9558dea7d729c9d9ac0e773e7b0c0477129b
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-14 03:37:09 +0300

    Completed The Footer Search Functionality

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,36 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatDivider } from '@angular/material/divider';
-import {
-	MatAutocomplete,
-	MatAutocompleteTrigger,
-	MatOption,
-} from '@angular/material/autocomplete';
 import { TitleCasePipe } from '@angular/common';
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [
-		TitleCasePipe,
-		RouterLink,
-		MatDivider,
-		MatAutocomplete,
-		MatAutocompleteTrigger,
-		MatOption,
-	],
+	imports: [TitleCasePipe, RouterLink, MatDivider],
 	templateUrl: './footer.component.html',
 	styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-	searching: boolean = false;
 	footerLinks: string[] = ['home', 'users'];
-	options: string[] = ['One', 'Two', 'Three'];
+
+	constructor(private router: Router) {}
 
 	onSearchSubmit(searchValue: string) {
-		console.log(searchValue);
-	}
-	onSearchChange(searchValue: string) {
-		console.log(searchValue);
+		this.router.navigateByUrl(`/users/${searchValue}`);
 	}
 }
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,31 +1,14 @@
 import { Component } from '@angular/core';
-import { MatInput } from '@angular/material/input';
-import {
-	MatAutocomplete,
-	MatAutocompleteTrigger,
-	MatOption,
-} from '@angular/material/autocomplete';
-import { Router, RouterLink } from '@angular/router';
-import { TitleCasePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [
-		RouterLink,
-		TitleCasePipe,
-		MatInput,
-		MatAutocomplete,
-		MatAutocompleteTrigger,
-		MatOption,
-	],
+	imports: [],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-	searching: boolean = false;
-	options: string[] = ['One', 'Two', 'Three'];
-
 	constructor(private router: Router) {}
 
 	onSearchSubmit(searchValue: string) {
