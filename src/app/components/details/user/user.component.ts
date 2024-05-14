87e5b1e3639ac749300dea6de8d1f7530e82ba72
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-14 03:00:49 +0300

    Completed The Details Page's User Component

--- src/app/components/details/user/user.component.ts
+++ src/app/components/details/user/user.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user/user.service';
+import { TUser } from '../../../types/user';
+import { ActivatedRoute } from '@angular/router';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
 	selector: 'app-user',
 	standalone: true,
-	imports: [],
+	imports: [MatDivider],
 	templateUrl: './user.component.html',
 	styleUrl: './user.component.scss',
 })
 export class UserComponent implements OnInit {
-	constructor(private userService: UserService) {}
+	user: TUser | null = null;
+
+	constructor(
+		private userService: UserService,
+		private activatedRoute: ActivatedRoute
+	) {}
 
 	ngOnInit(): void {
-		this.userService
-			.getUserDetails(5)
-			.subscribe((response) => console.log(response));
+		this.getUserIdFromParams();
+	}
+
+	getUser(id: number): void {
+		this.userService.getUserDetails(id).subscribe((response) => {
+			this.user = response.data;
+		});
+	}
+
+	getUserIdFromParams(): void {
+		this.activatedRoute.params.subscribe((params) => {
+			this.getUser(params['id']);
+		});
 	}
 }
