cf91a5dc971f0ba8fbee103919d086aa0a1a0639
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-15 11:43:40 +0300

    Adjusted Cache Interceptor

--- src/app/middlewares/cache.interceptor.ts
+++ src/app/middlewares/cache.interceptor.ts
@@ -1,35 +1,20 @@
 import { HttpHandlerFn, HttpRequest, HttpResponse } from '@angular/common/http';
-import { of, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
-export function cacheInterceptor(
+export const cacheInterceptor = (
 	request: HttpRequest<any>,
 	next: HttpHandlerFn
-) {
-	const baseUrl: string = 'https://reqres.in/api';
-	const cache = new Map<string, HttpResponse<any>>();
-	const endpoints = new Set<string>(['/users?page=1', '/users?page=2']);
-	const url = request.url.replace(baseUrl, '');
-
-	if (endpoints.has(url)) {
-		const cachedResponse = cache.get(request.url);
-
-		if (cachedResponse) {
-			return of(cachedResponse);
-		}
-
-		return next(request).pipe(
-			tap((response) => {
-				if (response instanceof HttpResponse) {
-					cache.set(request.url, response);
-				}
-				return response;
-			}),
-			catchError((error) => {
-				return throwError(error);
-			})
-		);
-	}
-
-	return next(request);
-}
+) => {
+	return next(request).pipe(
+		tap((response) => {
+			if (response instanceof HttpResponse) {
+				localStorage.setItem(request.url, JSON.stringify(response));
+			}
+			return response;
+		}),
+		catchError((error) => {
+			return throwError(() => new Error(error));
+		})
+	);
+};
