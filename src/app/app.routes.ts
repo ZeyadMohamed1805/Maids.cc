802fae8738ca876fc5046eec2a4f4a3e37788f27
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-15 14:18:54 +0300

    Completed The Not Found Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './pages/users/users.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'users' },
 	{ path: 'users', component: UsersComponent },
 	{ path: 'users/:id', component: DetailsComponent },
+	{ path: '**', component: NotFoundComponent },
 ];
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+	selector: 'app-not-found',
+	standalone: true,
+	imports: [],
+	templateUrl: './not-found.component.html',
+	styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+	message: string | null = null;
 
+	constructor(private activatedRoute: ActivatedRoute) {}
+
+	ngOnInit(): void {
+		this.setMessage(this.getUserParam());
+	}
+
+	getUserParam(): string | undefined {
+		return this.activatedRoute.snapshot.params['users'];
+	}
+
+	setMessage(userParam: string | undefined): void {
+		if (userParam) this.message = `There Is No User With Id ${userParam}`;
+		else {
+			this.message = 'The Page You Are Requesting Does Not Exist';
+		}
+	}
 }
